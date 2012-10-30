0c1d49b522e971fa61542c2867285239ea32403f
Mark Quiel <mark@ooozeinc.com>
2012-10-30 11:33:39 +0800

    Added Rails Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlingnenaApp::Application.config.secret_token = '55b22557b85adc65a23e53c55062e951c81582a263a724b63f2e771bd6864d92beeefa19ca0f3131af31cfcb7eba65586fd66f1da263077ce6d3b986f41ac4d1'
